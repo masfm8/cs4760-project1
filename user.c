d1274e2ea747a820b33de4f66bae6e03857f84e5
MD SARKER <131039792+masfm8@users.noreply.github.com>
2024-09-10 02:23:34 -0500

    Update user.c

--- user.c
+++ user.c
@@ -4,16 +4,12 @@
 #include <sys/types.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
+    if (argc < 2) {
         fprintf(stderr, "Usage: %s <iterations>\n", argv[0]);
         return 1;
     }
 
     int iterations = atoi(argv[1]);  // Convert the number of iterations from string to integer
-    if (iterations <= 0) {
-        fprintf(stderr, "Error: Iterations must be a positive integer\n");
-        return 1;
-    }
 
     for (int i = 0; i < iterations; i++) {
         printf("USER PID: %d, PPID: %d, Iteration: %d before sleeping\n", getpid(), getppid(), i + 1);
